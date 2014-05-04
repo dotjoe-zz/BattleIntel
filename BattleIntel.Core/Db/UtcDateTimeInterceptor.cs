41e6a5badbc243158fd919d615cc74be166bd4f9
dotjoe <joe.marquardt@gmail.com>
2014-05-04 06:35:36 -0500

    UTCDate interceptor should extend EmptyInterceptor

--- BattleIntel.Core/Db/UtcDateTimeInterceptor.cs
+++ BattleIntel.Core/Db/UtcDateTimeInterceptor.cs
@@ -4,21 +4,21 @@ using NHibernate.Type;
 
 namespace BattleIntel.Core.Db
 {
-    class UtcDateTimeInterceptor : IInterceptor
+    class UtcDateTimeInterceptor : EmptyInterceptor
     {
-        public bool OnLoad(object entity, object id, object[] state, string[] propertyNames, IType[] types)
+        public override bool OnLoad(object entity, object id, object[] state, string[] propertyNames, IType[] types)
         {
             ConvertDatabaseDateTimeToUtc(state, types);
             return true;
         }
 
-        public bool OnSave(object entity, object id, object[] state, string[] propertyNames, IType[] types)
+        public override bool OnSave(object entity, object id, object[] state, string[] propertyNames, IType[] types)
         {
             ConvertLocalDateToUtc(state, types);
             return true;
         }
 
-        public bool OnFlushDirty(object entity, object id, object[] currentState, object[] previousState, string[] propertyNames, IType[] types)
+        public override bool OnFlushDirty(object entity, object id, object[] currentState, object[] previousState, string[] propertyNames, IType[] types)
         {
             ConvertLocalDateToUtc(currentState, types);
             return true;
@@ -66,24 +66,5 @@ namespace BattleIntel.Core.Db
                 ++index;
             }
         }
-
-        public void OnDelete(object entity, object id, object[] state, string[] propertyNames, IType[] types) { }
-        public int[] FindDirty(object entity, object id, object[] currentState, object[] previousState, string[] propertyNames, IType[] types) { return null; }
-        public object Instantiate(Type type, object id) { return null; }
-        public object IsUnsaved(object entity) { return null; }
-        public void PreFlush(System.Collections.ICollection entities) { }
-        public void PostFlush(System.Collections.ICollection entities) { }
-        public void AfterTransactionBegin(ITransaction tx) { }
-        public void AfterTransactionCompletion(ITransaction tx) { }
-        public void BeforeTransactionCompletion(ITransaction tx) { }
-        public object GetEntity(string entityName, object id) { return null; }
-        public string GetEntityName(object entity) { return null; }
-        public object Instantiate(string entityName, EntityMode entityMode, object id) { return null; }
-        public bool? IsTransient(object entity) { return null; }
-        public void OnCollectionRecreate(object collection, object key) { }
-        public void OnCollectionRemove(object collection, object key) { }
-        public void OnCollectionUpdate(object collection, object key) { }
-        public NHibernate.SqlCommand.SqlString OnPrepareStatement(NHibernate.SqlCommand.SqlString sql) { return sql; }
-        public void SetSession(ISession session) { }
     }
 }
